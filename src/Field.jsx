48894c79bd9e0d105b2ee669f98532f76600c269
Marta Girdea <marta@phenotips.org>
2019-10-10 14:06:27 -0400

    Issue #1 - Addressing review comments: removed unnecessary field attributes

--- src/Field.jsx
+++ src/Field.jsx
@@ -24,11 +24,7 @@ export default function Field (props) {
                 fullWidth
                 error = {props.error}
                 onChange = {handleChange}
-                inputProps = {{
-                    name: 'val',
-                    id: props.metadata.name
-                }}>
-                    <MenuItem value = ""></MenuItem>
+            >
                     <MenuItem value={1}>Yes</MenuItem>
                     <MenuItem value={0}>No</MenuItem>
             </Select>
@@ -43,10 +39,7 @@ export default function Field (props) {
                 fullWidth
                 error = {props.error}
                 onChange = {handleChange}
-                inputProps = {{
-                    name: 'val',
-                    id: props.metadata.name
-                }}>
+            >
                     {props.metadata.values ? props.metadata.values.map( (entry, idx) =>
                         <MenuItem value={entry.value} key= {props.metadata.name + "_Option_" + idx}>{entry.label}</MenuItem>
                     ) : ""}
@@ -59,11 +52,9 @@ export default function Field (props) {
                 value={props.value}
                 variant="outlined"
                 fullWidth
-                id = {props.metadata.name}
                 type = "number"
                 onChange = {handleChange}
                 error = {props.error}
-                width = "auto"
                 InputProps = {{
                     endAdornment: <InputAdornment position="end">{(props.metadata.unit || "")}</InputAdornment>
                 }}
