f38bce0e6a9ab461aef2bbefef4974eda638dfc3
Marta Girdea <marta@phenotips.org>
2019-10-16 15:56:54 -0400

    Issue #1 - Improved how the app info is displayed

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,20 +1,18 @@
 import {
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
     Grid,
     makeStyles,
     Tooltip,
     Typography
 } from '@material-ui/core';
 import InfoIcon from '@material-ui/icons/Info';
-import ReactDOM from "react-dom";
 import React from "react";
 
-const useTooltipStyles = makeStyles(theme => ({
-    tooltip: {
-        color: "white",
-        backgroundColor: "black",
-        fontSize: 14
-    }
-}));
 const useStyles = makeStyles({
   hccHeading: {
     fontSize: '1.5rem',
@@ -33,20 +31,43 @@ const useStyles = makeStyles({
 export default function Header (props) {
     const classes = useStyles();
 
+    const [infoDisplayed, setInfoDisplayed] = React.useState(false);
+
+    const handleOpen = () => {
+        setInfoDisplayed(true);
+    };
+
+    const handleClose = () => {
+        setInfoDisplayed(false);
+    };
     return (
-        <Grid container>
+        <Grid container alignItems="center" justify="space-between" direction="row">
             <Grid item xs={"auto"}>
                 <Typography variant="h1" gutterBottom className={classes.hccHeading}>{props.text}</Typography>
             </Grid>
             {props.info &&
-            <Grid item xs>
+            <Grid item xs={false}>
                 <Tooltip
-                    enterTouchDelay={0}
                     title={props.info}
                     placement="bottom-start"
-                    classes={useTooltipStyles()}>
-                        <InfoIcon />
+                >
+                        <InfoIcon color="primary" fontSize="large" onClick={handleOpen}/>
                 </Tooltip>
+               <Dialog
+                 open={infoDisplayed}
+                 onClose={handleClose}
+                 scroll="paper"
+               >
+                 <DialogTitle>{props.text}</DialogTitle>
+                 <DialogContent dividers>
+                   <DialogContentText color="primary">
+                      {props.info}
+                   </DialogContentText>
+                 </DialogContent>
+                  <DialogActions>
+                    <Button onClick={handleClose} color="default">Close</Button>
+                  </DialogActions>
+                </Dialog>
             </Grid>
             }
         </Grid>);
