5d2d4fb2ba7b93e25e51f9ce407ae34b0987f592
conor <conorklamann@gmail.com>
2021-05-13 15:50:18 -0400

    update label and show results as percentage chance of recurrence

--- src/model.jsx
+++ src/model.jsx
@@ -3,31 +3,34 @@ import React from "react";
 const modelData = require("./model.json");
 
 function dot_product(a, b) {
-  var result = 0;
-  if (a.length == b.length) {
-    for (var i = 0; i < a.length; ++i) {
-      result += a[i] * b[i];
+    var result = 0;
+    if (a.length == b.length) {
+        for (var i = 0; i < a.length; ++i) {
+            result += a[i] * b[i];
+        }
     }
-  }
-  return result;
+    return result;
 }
 
 const model = {
-  sample_values: modelData.sample_values,
+    sample_values: modelData.sample_values,
 
-  variables: modelData.variables,
+    variables: modelData.variables,
 
-  estimate: function (data) {
-    const intermediate = dot_product(
-      data.map((v) => (v.value - v.mean) / v.sd),
-      data.map((v) => v.weight)
-    );
+    estimate: function (data) {
+        const intermediate = dot_product(
+            data.map((v) => (v.value - v.mean) / v.sd),
+            data.map((v) => v.weight)
+        );
 
-    return modelData.baseline_survival.map((s) => ({
-      label: s.timepoint,
-      survival: Math.pow(s.baseline_survival, Math.exp(intermediate)).toFixed(6),
-    }));
-  },
+        return modelData.baseline_survival.map((s) => ({
+            label: s.timepoint,
+            survival: `${(
+                (1 - Math.pow(s.baseline_survival, Math.exp(intermediate))) *
+                100
+            ).toFixed(3)}%`,
+        }));
+    },
 };
 
 export default model;
